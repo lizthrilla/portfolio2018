0748a05e69b98429b13a10897ba04e6eeb9a4f42
Liz Tiller <lizthrilla@users.noreply.github.com>
2019-01-18 23:46:58 -0500

    link and descriptions

--- src/components/Conferences.js
+++ src/components/Conferences.js
@@ -22,22 +22,24 @@ const Conferences = () => (
             <em className="date">January 19, 2019</em>
           </p>
           <p>
-            Discussing the short comings of React Native for android development and ways my team worked around it.
+            "React Native is advertised as a framework for developing native apps with javascript. Roughly 90% of the time, my team is able to work strictly in javascript, with only occassional trips into native code. In particular, my team has found our Android build to require more care than its iOS counterpart. This talk will look at the ways in which we have learned, sometimes the hard way, how to develop a react native app for Android, as well as my experience as a javascript developer being thrust into the native world. In particular, I will discuss our use of the react native device detection package to work through our Android style audits and ways we have worked around react native's shortcomings."
           </p>
         </div>
       </div>
 
       <div className="row item">
         <div className="twelve columns">
-          <h3><a href="https://startupweektampabay.com/" target="_blank">Tech Stars Startup Week Tampa Bay 2019</a></h3>
+          <h3><a href="https://startupweektampabay.com/" target="_blank">Startup Week Tampa Bay 2019</a></h3>
           <p className="info">
-            Panel: Web Accessibility
+            <a href="https://sched.co/KBl9">
+              Inclusive Design and Development with Web Accessibility Panel
+            </a>
             <span>&bull;</span>
             <em className="date">February 4, 2019</em>
           </p>
 
           <p>
-            Particpating in a panel on the importance of web accessibility.
+            "The focus on improving web accessibility has indavertently shifted to avoiding lawsuits when, more importantly, on the other end of the screen is a user base that is being excluded. In this panel discussion, you'll hear from two front-end developers/accessibility advocates and two assistive technology users to hear how incorporating a focus on web accessibility can transform your users' experiences for the better."
           </p>
         </div>
       </div>
